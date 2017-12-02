a6ff80d977207ce13c6e7d67e4804c69edca0271
Cynthia London <cynthe17@gmail.com>
2017-12-01 19:58:40 -0600

    empty

--- assets/javascript/engine.js
+++ assets/javascript/engine.js
@@ -4,3 +4,20 @@
 //array
 
 var movies = ["Cats!", "Dogs!", "Elepants!", "Monkeys!", "Skunks!"];
+
+
+//movie data function
+
+function renderButtons() {
+
+	//deletes buttons
+
+	$('moviesView').empty();
+
+
+	//loop
+	for (var i = Things.length - 1; i >= 0; i--) {
+		Things[i]
+	}
+
+}

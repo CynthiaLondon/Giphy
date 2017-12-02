ed2693def3ea4e1190bb546f83e32f46e66b84e5
Cynthia London <cynthe17@gmail.com>
2017-12-01 20:14:30 -0600

    buttongenerator

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,28 @@
+
+		//create buttons at top
+
+		$(document).ready(function(){
+
+
+			var animalsarray = ["cats", "dogs", "elephants", "monkeys", "skunks"];
+
+
+			function buttongenerator() {
+				for (var i = 0; i < animalsarray.length; i++ {
+
+					var name = animalsarray[i];
+					var button = $('<buttons>');
+					button.addClass("gif-button");
+					button.text(name);
+					button.attr("data-name", name);
+
+
+					$("#buttons").append(button);
+				
+			}
+
+		}
+
+		buttongenerator();
+
+		//create results when click on button
